ab238d2191b08f3b9b4afa77a3cdbd678c6bd011
AniamK <kathris@hotmail.co.th>
2017-06-14 15:27:55 +0700

    config database
    config database to research
    password => ''

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
 	'class' => 'yii\db\Connection',
-	'dsn' => 'mysql:host=localhost;dbname=yii2training',
+	'dsn' => 'mysql:host=localhost;dbname=research',
 	'username' => 'root',
-	'password' => 'root',
+	'password' => '',
 	'charset' => 'utf8',
 ];
